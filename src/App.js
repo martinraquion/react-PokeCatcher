53e9b1493902baf15c41abb025e2f50caf6df1a0
Martin Raquion <raquionearl@gmail.com>
2019-06-24 17:57:51 +0800

    Fetched Regions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import './App.css';
 
-
 import PokeHeader from './components/PokeHeader'
 import SelectLocation from './components/SelectLocation'
 import Encounter from './components/Encounter'
 import CapturedBox from './components/CapturedBox'
 
-
+import { pokeApi } from './config/axiosConfig'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      regions: [],
+      locations: [],
+      areas: []
+    }
+  }
+
+  componentDidMount(){
+    pokeApi
+      .get('region')
+      .then(res => {
+        this.setState({
+          regions: res.data.results
+        })
+      })
+    
+
+    }
+
   render(){
   return (
     <div className="App">
@@ -17,7 +37,10 @@ class App extends Component {
       <div className="main-content">
       <div className="main-wrapper">
       <div className="container-sm">
-      <SelectLocation />
+      <SelectLocation 
+      regions={this.state.regions} 
+      locations={this.state.locations} 
+      areas={this.state.areas}/>
       <Encounter />
       </div> 
       <div className="container-lg">
--- src/components/SelectLocation.js
+++ src/components/SelectLocation.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react'
+import React from 'react';
 
 
-export default class SelectLocation extends Component{
-    render(){
+function PokeSelector({items}){
+  return(
+    <select disabled={!items.length} className="select-inp"> 
+    {items.map(item=>(
+      <option key={item.url} value={item.name}>{item.name}</option>
+    ))}
+    </select> 
+  )
+}
+
+
+
+export default function SelectLocation({regions = [], locations = [], areas=[]}){
+    
     return(
       <div className="form-container">
       <div className="form-wrap">
           <label className="select-label">REGION: </label>
-          <select name="region" id="region-inp" className="select-inp"> 
-          </select> 
+          <PokeSelector items={regions}/>
           <label className="select-label">LOCATION: </label>
-          <select name="region" id="loc-inp" className="select-inp">
-          </select>
+          <PokeSelector items={locations}/>
           <label className="select-label area-label"> AREA: </label>
-          <select name="region" id="area-inp" className="select-inp">
-          </select>
+          <PokeSelector items={areas}/>
       </div>
       <div className="btn-wrap" id="btn-wrap">
           <button value="EXPLORE" className="explore-btn" id="explore-btn" disabled>EXPLORE</button>
       </div>
     </div>
     )
-    }
+    
   }
